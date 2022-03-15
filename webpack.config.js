fab14864717f3f6b8736b749a246aecf85c4aba2
Sergey Fedotov <fsa@tavda.info>
2022-03-16 03:24:25 +0700

    Чистка кода. Включено создание map файлов для development.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,5 @@
 "use strict";
 
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
@@ -45,7 +42,6 @@ module.exports = {
         ],
     },
     plugins: [
-        //new CleanWebpackPlugin(),
         new MiniCssExtractPlugin()
     ],
 };
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,15 +4,13 @@ const { merge } = require("webpack-merge");
 const baseWebpackConfig = require("./webpack.config");
 const devWebpackConfig = merge(baseWebpackConfig, {
     mode: "development",
-    //devtool: 'cheap-module-eval-source-map',
     devServer: {
-        //contentBase: baseWebpackConfig.externals.paths.dist,
         port: 3000
     },
     plugins: [
-        //new webpack.SourceMapDevToolPlugin({
-        //    filename: "[name][ext].map",
-        //}),
+        new webpack.SourceMapDevToolPlugin({
+            filename: "[name][ext].map",
+        }),
     ],
 });
 
